ba119e11898e579e6b06727a0c5a22b1cd2b10bf
pmijar <Prashanth.Mijar@gmail.com>
2023-10-25 14:16:16 -0400

    Updated explicit Timeout variable

--- src/main/java/Utils/Global_Variables.java
+++ src/main/java/Utils/Global_Variables.java
@@ -1,6 +1,6 @@
 package Utils;
 
 public class Global_Variables {
-    public static final int EXPLICIT_TIMEOUT = 10;
+    public static final int EXPLICIT_TIMEOUT = 9;
     public static final String BASE_URL = "http://www.webdriveruniversity.com";
 }
